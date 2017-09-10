2a68402259cff03426cdd716ca937379a896accc
namke <john@namke.com>
2017-09-10 22:30:21 +0100

    Updated calc.rb to generate the octave-lookup table directly into a header file which can be #included into the main file.

--- dr1/calc.rb
+++ dr1/calc.rb
@@ -1,13 +1,33 @@
-if ARGV.length == 2
+# We're going to generate an octave's worth of increments.
+# beginning by splitting an ocatve into steps
 
-  steps=ARGV[0].to_f
-  fracBits=ARGV[1].to_i
-  multiplier=1 << fracBits
-  puts "outputting #{steps} steps per octave"
- 
-  (0...steps).each do |n|
-    f = 2.0**(n/steps)
-    puts "0x#{(f*multiplier).to_i.to_s(16)} // #{n}: #{f} --> #{f*multiplier}"
+SRATE=32768
+WTSIZE=1024
+
+# root frequency is that of A0 (according to Dodge and Jerse pg.37)
+ROOT=27.50
+
+# the integer part of the sample increments in the table are no
+# larger than 1, so we can use 15 bits for the fraction
+FRACBITS = 15
+
+# 128 steps per octave means that the 10 bit ADC covers 4 octaves
+STEPS=128
+
+
+File.open("calc.h",'w') do |f|
+  f.puts "#define SRATE (#{SRATE})"
+  f.puts "#define WTSIZE (#{WTSIZE})"
+  f.puts "#define OCTSTEPS (#{STEPS})"
+  f.puts "#define NOTEMASK (0x#{(STEPS-1).to_s(16)})"
+  f.puts "const unsigned short octaveLookup[#{STEPS}] PROGMEM = {"
+
+  (0...STEPS).each do |n|
+    freq = ROOT * (2.0**(n.to_f/STEPS))
+    # Dodge & Jerse (pg.67) say that the sample increment is WTSIZE*(fo/fs)
+    si = (WTSIZE * freq)/SRATE
+    sifixed = ((WTSIZE << FRACBITS)* freq)/SRATE
+    f.puts "  0x#{sifixed.to_i.to_s(16)}, // #{freq} [#{si}]"
   end
+  f.puts "};"
 end
-
