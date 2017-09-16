1da0121c2002ca67ca9ff0d5668005cc4ec638bb
namke <john@namke.com>
2017-09-16 21:28:42 +0100

    Collapsing oscillator-specific stuff into a structure to streamline the code. Some of the maths might not be right, but code is tidier.

--- dr1/dr1.ino
+++ dr1/dr1.ino
@@ -1,38 +1,41 @@
 #include "calc.h"
-#define ADC_CHANS (2)
-
-// This means that we want to have an indexer with 10 bits of integer
-// and 'some' bits of fraction. Not sure whether 6 is enough, but we'll
-// start with that.
-unsigned short idx = 0;
-unsigned short idx2 = 0;
+#define NUM_OSCS (2)
 
 #define F_TIM (8000000L)
 
 // Remember(!) the input clock is 64MHz, therefore all rates
 // are relative to that.
-#if ((F_TIM/(SRATE*ADC_CHANS)) < 255)
-#define T1_MATCH ((F_TIM/(SRATE*ADC_CHANS))-1)
+#if ((F_TIM/(SRATE*NUM_OSCS)) < 255)
+#define T1_MATCH ((F_TIM/(SRATE*NUM_OSCS))-1)
 #define T1_PRESCALE (_BV(CS12))  //prescaler clk/8 (i.e. 8MHz)
 #else
-#define T1_MATCH (((F_TIM/8L)/(SRATE*ADC_CHANS))-1)
+#define T1_MATCH (((F_TIM/8L)/(SRATE*NUM_OSCS))-1)
 #define T1_PRESCALE (_BV(CS12) | _BV(CS11) | _BV(CS10))  //prescaler clk/64 (i.e. 1MHz)
 #endif
 
-unsigned char muxVals[4] = {  _BV(MUX1),               // mux: ADC2
-                              _BV(MUX1) | _BV(MUX0),   // mux: ADC3
-                              _BV(MUX0),               // mux: ADC1
-                              0,                       // mux: ADC0
-                            };
+typedef struct {
+  // This means that we want to have an indexer with 10 bits of integer
+  // and 'some' bits of fraction. Not sure whether 6 is enough, but we'll
+  // start with that.
+  unsigned short    phase;
+  unsigned short    adcVal;
+  unsigned short    muxVal;
+  volatile uint8_t  *outputReg;
+} oscStructDyn;
 
-unsigned short adcVals[4];
+oscStructDyn osc[NUM_OSCS];
 
 void setup()
 {
-  PLLCSR |= _BV(PLLE);               // Enable 64 MHz PLL
-  delayMicroseconds(100);            // Stabilize
-  while (!(PLLCSR & _BV(PLOCK)));    // Wait for it...
-  PLLCSR |= _BV(PCKE);               // Timer1 source = PLL
+  PLLCSR |= _BV(PLLE);                // Enable 64 MHz PLL
+  delayMicroseconds(100);             // Stabilize
+  while (!(PLLCSR & _BV(PLOCK)));     // Wait for it...
+  PLLCSR |= _BV(PCKE);                // Timer1 source = PLL
+
+  osc[0].outputReg = &OCR0A;                // Physical pin 5
+  osc[0].muxVal =   _BV(MUX1);              // ADC2 Physical pin 3
+  osc[1].outputReg = &OCR0B;                // Physical pin 6
+  osc[1].muxVal =   _BV(MUX1) | _BV(MUX0);  // ADC3 Physical pin 2
 
   ///////////////////////////////////////////////
   // Set up Timer1 for the sample-update ISR since we want to
@@ -59,7 +62,7 @@ void setup()
 
   ///////////////////////////////////////////////
   // Set up the ADC (A3 for starters)
-  ADMUX  = muxVals[0];
+  ADMUX  = osc[0].muxVal;
   ADCSRA = _BV(ADEN) |_BV(ADSC);  // enable the ADC and start a conversion
   DIDR0 = _BV(ADC3D);             // disable digital pin attached to ADC3
   // read from analogue on (chip pin 2)
@@ -68,52 +71,33 @@ void setup()
   pinMode(A2, INPUT);
 }
 
-unsigned char octave;
-unsigned char note;
-unsigned char octave2;
-unsigned char note2;
-
 void loop() {
 }
 
-unsigned char adc_read=0;
+unsigned char oscNum=0;
 
 ISR(TIMER1_COMPA_vect)
 {
+  oscStructDyn* currentOsc=&osc[oscNum];
+  unsigned char octave;
+  unsigned char note;
+  
   // look up the wave based on our (rounded) index, and 
   // PWM with the 'integer' part,
-  unsigned short oldIdx = idx;
-  adcVals[adc_read] = ADCL|(ADCH << 8);
+  currentOsc->adcVal = ADCL|(ADCH << 8);
   ADCSRA = 0;   // stop the converter
 
-  adc_read++;
-  adc_read %= ADC_CHANS;
-  
+  octave = 1 << (currentOsc->adcVal >> 7); // leaves us with 3-bits of octave range (1-8)
+  note = currentOsc->adcVal & NOTEMASK;
+
+  *currentOsc->outputReg = pgm_read_byte(&wave[(currentOsc->phase+0x20) >> 6]);
+  currentOsc->phase += octave * (pgm_read_byte(&octaveLookup[note]));
+
+  oscNum++;
+  oscNum %= NUM_OSCS;
+
   // Start the ADC off again
-  ADMUX  = muxVals[adc_read];
+  ADMUX  = osc[oscNum].muxVal; // select the correct channel for the next conversion
   ADCSRA = _BV(ADEN) |_BV(ADSC);
-
-  unsigned short freqCtrl = adcVals[1]; //analogRead(A3) & 0x03ff;
-  octave = 1 << (freqCtrl >> 7); // leaves us with 3-bits of octave range (1-8)
-  note = freqCtrl & NOTEMASK;
-  unsigned short ctrl2  = adcVals[0]; // analogRead(A1) & 0x03ff;
-  octave2 = 1 << (ctrl2 >> 7); // leaves us with 3-bits of octave range (1-8)
-  note2 = ctrl2 & NOTEMASK;
-
-  // Oscillator 1
-  OCR0A = pgm_read_byte(&wave[(idx+0x20) >> 6]);
-
-  // Oscillator 2 (hard sync-ed to Osc1)
-  OCR0B = pgm_read_byte(&wave[(idx2+0x20) >> 6]);
-
-  idx += octave * (pgm_read_byte(&octaveLookup[note]));
-  idx2 += octave2 * (pgm_read_byte(&octaveLookup[note2]));
-
-//  // hard sync oscillator 2
-//  if (idx < oldIdx)
-//  {
-//    idx2=0;
-//  }
-
 }
 
